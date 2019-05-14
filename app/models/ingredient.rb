c02e96f578a75efba2ccbbb1a594d4b88b128627
Liam Garrison <liamgarrison@gmail.com>
2019-05-14 12:37:17 +0100

    added associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  validates :name, presence: true
+  has_many :cocktails,
+           through: :doses
+  validates :name, presence: true, uniqueness: true
 end
