cb95bc5b68e4916a8010ba5d8ec26169a4c4b80b
Liam Garrison <liamgarrison@gmail.com>
2019-05-14 13:13:30 +0100

    added actions to controller and new and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
   resources :cocktails, only: [:index, :show, :new, :create]
 end
