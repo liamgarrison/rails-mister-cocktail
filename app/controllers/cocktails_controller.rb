e07c8ae308ef31b056e831ecf379f31df908640f
Liam Garrison <liamgarrison@gmail.com>
2019-05-14 15:32:53 +0100

    added homepage styling

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :image_url)
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients,
            through: :doses
   validates :name, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :image_url, presence: true
 end
