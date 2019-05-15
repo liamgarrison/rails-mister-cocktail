418a821e6baa64928535d2acea19693c43fac99c
Liam Garrison <liamgarrison@gmail.com>
2019-05-15 14:22:41 +0100

    added reviews controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
